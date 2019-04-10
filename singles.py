b7f6cc44e67e4bf4abfc3a133a2c66cfc31178b3
Sean R Reid <sean@torchcodelab.com>
2019-04-10 11:51:43 -0400

    * Removed unecessary imports * Cleaned up formatting (it was a little messy) * Added comments (b/c, instructions are helpful!) * Returned a more user-friendly message about the number one song, with a friendly formatted date

--- singles.py
+++ singles.py
@@ -2,15 +2,38 @@ import sys
 import datetime
 import billboard
 
-PLAYLIST_NAME = '#1 Singles for 1984'
-CHART         = 'hot-100'
-NOW           = datetime.datetime.now()
-START_DATE    = NOW.strftime("1984-%m-%d")
+YEAR    = input("Which year do you want to look up? (leave blank for 1984) : ") or "1984"
+CHART   = 'hot-100'
+NOW = datetime.datetime.now().strftime("%m-%d")
+
+"""  
+String Concatenation
+The START_DATE variable has to be properly formatted 
+before it is passed into billboard.ChartData()
+""" 
+START_DATE  = YEAR + '-' + NOW
+
+"""  
+Create a Readable/User Friendly Date String
+This will be printed out in the result
+
+First, we have to convert our string into a proper date object...
+"""  
+DATE_OBJECT     = datetime.datetime.strptime(START_DATE, '%Y-%m-%d')
+"""
+Next, we convert our DATE_OBJECT into a string value that we will interpolate later...
+"""
+READABLE_DATE   = datetime.datetime.strftime(DATE_OBJECT,'%B %d, %Y')
 
 def number_one_single():
     chart = billboard.ChartData(CHART, date=START_DATE)
     number_one = chart[0]
-    print (number_one)
+    """     
+    String Interpolation
+    %s = string
+    %d = integer 
+    """
+    print ('The #1 single for %s is %s' % (READABLE_DATE, number_one))
 
 def main():
     number_one_single()
